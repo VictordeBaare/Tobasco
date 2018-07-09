348cc8fcb6b8c101e7cc1b2c535c391a2ea739c7
Pavel Prokharau <pavelprokharau@dsw.nl>
2018-07-09 12:49:27 +0200

    method TableHelper.GetParameters() set to virtual

--- Tobasco/Model/Builders/DatabaseBuilders/TableHelper.cs
+++ Tobasco/Model/Builders/DatabaseBuilders/TableHelper.cs
@@ -1,50 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Tobasco.Constants;
 using Tobasco.Enums;
 using Tobasco.Templates;
 
 namespace Tobasco.Model.Builders.DatabaseBuilders
 {
-    public class TableHelper : DatabaseHelper
-    {
-        public TableHelper(Entity entity, Database database) : base(entity, database)
-        {
-        }
+	public class TableHelper : DatabaseHelper
+	{
+		public TableHelper(Entity entity, Database database) : base(entity, database)
+		{
+		}
 
-        protected TemplateParameter GetParameters()
-        {
-            var parameters = new TemplateParameter();
-            parameters.Add(SqlConstants.TableName, Name);
-            parameters.Add(SqlConstants.TableProperties, GetTableProperties());
-            parameters.Add(SqlConstants.Constraints, GetContraints());          
+		protected virtual TemplateParameter GetParameters()
+		{
+			var parameters = new TemplateParameter();
+			parameters.Add(SqlConstants.TableName, Name);
+			parameters.Add(SqlConstants.TableProperties, GetTableProperties());
+			parameters.Add(SqlConstants.Constraints, GetContraints());
 
-            return parameters;
-        }        
+			return parameters;
+		}
 
-        private string GetContraints()
-        {
-            var builder = new StringBuilder();
-            foreach (var sqlprop in GetChildProperties)
-            {
-                var constraint = sqlprop.SelectChildForeignkeyConstraint(Name);
-                if (!string.IsNullOrEmpty(constraint))
-                {
-                    builder.AppendLine($",{constraint}");
-                }
-            }
-            foreach (var sqlprop in Entity.GetSqlProperties.Where(x => x.Property.DataType.Datatype == Datatype.Reference))
-            {
-                var constraint = sqlprop.SelectReferenceConstraint(Name);
-                if (!string.IsNullOrEmpty(constraint))
-                {
-                    builder.AppendLine($",{constraint}");
-                }
-            }
-            return builder.ToString();
-        }
-    }
+		private string GetContraints()
+		{
+			var builder = new StringBuilder();
+			foreach (var sqlprop in GetChildProperties)
+			{
+				var constraint = sqlprop.SelectChildForeignkeyConstraint(Name);
+				if (!string.IsNullOrEmpty(constraint))
+				{
+					builder.AppendLine($",{constraint}");
+				}
+			}
+			foreach (var sqlprop in Entity.GetSqlProperties.Where(x => x.Property.DataType.Datatype == Datatype.Reference))
+			{
+				var constraint = sqlprop.SelectReferenceConstraint(Name);
+				if (!string.IsNullOrEmpty(constraint))
+				{
+					builder.AppendLine($",{constraint}");
+				}
+			}
+			return builder.ToString();
+		}
+	}
 }
